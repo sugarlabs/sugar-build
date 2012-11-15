99b13c2bd7a90a439cb477b13ca25bf63d0ef1c0
Daniel Narvaez <dwnarvaez@gmail.com>
2012-11-15 17:29:02 +0100

    Move configuration files to their own dir

--- commands/common.py
+++ commands/common.py
@@ -9,7 +9,7 @@ from devbot import system
 from devbot import config
 
 def setup():
-    config.set_config_dir(os.path.join(base_path, "scripts"))
+    config.set_config_dir(os.path.join(base_path, "config"))
     config.set_install_dir(os.path.join(base_path, "install"))
     config.set_source_dir(os.path.join(base_path, "source"))
     config.set_build_dir(os.path.join(base_path, "build"))
