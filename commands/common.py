8956a666c3fb455de4cdb167611bc478e20948da
Daniel Narvaez <dwnarvaez@gmail.com>
2012-11-23 19:29:55 +0100

    Fix shell prompt string on ubuntu
    It was getting overwritten by ~/.bashrc

--- commands/common.py
+++ commands/common.py
@@ -1,22 +1,23 @@
 import os
 import sys
 
-base_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+base_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+helpers_dir = os.path.join(base_dir, "commands", "helpers")
 
-sys.path.append(base_path)
+sys.path.append(base_dir)
 
 from devbot import system
 from devbot import config
 from devbot import distro
 
 def setup():
-    config.set_config_dir(os.path.join(base_path, "config"))
-    config.set_install_dir(os.path.join(base_path, "install"))
-    config.set_source_dir(os.path.join(base_path, "source"))
-    config.set_build_dir(os.path.join(base_path, "build"))
-    config.set_commands_dir(os.path.join(base_path, "commands"))
-    config.set_logs_dir(os.path.join(base_path, "logs"))
-    config.set_prefs_path(os.path.join(base_path, "prefs"))
+    config.set_config_dir(os.path.join(base_dir, "config"))
+    config.set_install_dir(os.path.join(base_dir, "install"))
+    config.set_source_dir(os.path.join(base_dir, "source"))
+    config.set_build_dir(os.path.join(base_dir, "build"))
+    config.set_commands_dir(os.path.join(base_dir, "commands"))
+    config.set_logs_dir(os.path.join(base_dir, "logs"))
+    config.set_prefs_path(os.path.join(base_dir, "prefs"))
 
     version = distro.get_system_version()
 
--- devbot/shell.py
+++ devbot/shell.py
@@ -3,9 +3,10 @@
 import os
 
 from devbot import environ
+from devbot import config
 
-def start():
+def start(rcfile):
     environ.setup()
 
-    user_shell = os.environ.get("SHELL", "/bin/sh")
-    os.execlp(user_shell, user_shell)
+    bash_path = "/bin/bash"
+    os.execlp(bash_path, bash_path, "--rcfile", rcfile)
