e488ee4e2333a0e1814e2ef75141df06ab7226e4
Daniel Narvaez <dwnarvaez@gmail.com>
2012-11-26 17:20:07 +0100

    Improve the documentation

--- devbot/plugins/interfaces.py
+++ devbot/plugins/interfaces.py
@@ -5,48 +5,53 @@ from devbot import command
 from devbot import distro
 
 class PackageManager:
+    """Package management"""
+
     def install_packages(self, packages):
-        """Install packages
+        """Install packages.
 
            :param packages: packages to install
         """
 
     def remove_packages(self, packages):
-        """Remove a list of packages
+        """Remove a list of packages.
 
            :param packages: packages to remove
         """
 
     def update(self):
-        """Update packages to the latest version"""
+        """Update packages to the latest version."""
 
     def find_all(self):
-        """Return all the installed packages"""
+        """Return all the installed packages."""
 
     def find_with_deps(self, package_names):
         """Return all the installed dependencies of a list of packages.
            The packages itself are also returned in the list.
 
-           :param package_names: names of the packages to find
-           :returns packages with all their dependencies
+           :param packages_name: names of the packages to find
+           :returns: list of packages with all the dependencies
         """
 
 class DistroInfo:
-    """Informations about the distribution
-    """
+    """Informations about the distribution"""
 
     def __init__(self):
         self.name = None
-        """Name"""
+        """The distribution name."""
 
         self.version = None
-        """Version"""
+        """The distribution version."""
     
         self.system_version = None
-        """Distro independent system version"""
+        """A distribution independent system version, normally the same
+           major version of GNOME installed on the system.
+        """
 
         self.valid = False
-        """If valid we running on this distribution"""
+        """If set to True we are running on this distribution and the
+           attributes are all valid.
+        """
 
         self.use_lib64 = False
-        """Install libraries in the lib64 directory"""
+        """If set to True install libraries in the lib64 directory."""
