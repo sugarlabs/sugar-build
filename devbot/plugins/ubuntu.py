83ae400692b63e5d06d551621f8970a1f7ca4f7a
Daniel Narvaez <dwnarvaez@gmail.com>
2013-04-29 23:03:42 +0200

    Disable more modules on raring

--- devbot/plugins/ubuntu.py
+++ devbot/plugins/ubuntu.py
@@ -42,10 +42,8 @@ class DistroInfo(interfaces.DistroInfo):
 
         self.version = os_info.get("VERSION_ID", None)
 
-        if self.version == "12.10":
+        if self.version == "12.10" or self.version == "13.04":
             self.gnome_version = "3.6"
-        elif self.version == "13.04":
-            self.gnome_version = "3.8"
         else:
             self.supported = False
 
