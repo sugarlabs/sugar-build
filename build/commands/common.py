66ad642a82594930a351a65b99429bb068bb8905
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-23 02:07:05 +0200

    Rework logging

--- build/commands/common.py
+++ build/commands/common.py
@@ -1,8 +1,11 @@
+import logging
+from logging.handlers import RotatingFileHandler
 import os
 import sys
 
 build_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 commands_dir = os.path.join(build_dir, "commands")
+logs_dir = os.path.join(build_dir, "logs")
 root_dir = os.path.dirname(build_dir)
 
 from osbuild import main
@@ -13,17 +16,13 @@ def is_buildbot():
     return "SUGAR_BUILDBOT" in os.environ
 
 
-def get_config_args(log_name=None):
+def get_config_args():
     config_args = {"config_dir": os.path.join(build_dir, "config"),
                    "install_dir": os.path.join(build_dir, "out", "install"),
                    "source_dir": os.path.join(root_dir),
                    "docs_dir": os.path.join(build_dir, "out", "docs"),
                    "state_dir": os.path.join(build_dir, "state"),
-                   "prefs_path": os.path.join(root_dir, "prefs"),
-                   "logs_dir": os.path.join(build_dir, "logs")}
-
-    if log_name:
-        config_args["log_name"] = log_name
+                   "prefs_path": os.path.join(root_dir, "prefs")}
 
     if is_buildbot():
         config_args["git_user_name"] = "buildbot"
@@ -36,10 +35,27 @@ def print_close_message():
     print "Type Shift-Alt-Q inside sugar to close."
 
 
-def setup(log_name=None, check_args={}):
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    log_path = os.path.join(logs_dir, "sugar-build.log")
+
+    try:
+        os.makedirs(logs_dir)
+    except OSError:
+        pass
+
+    handler = RotatingFileHandler(log_path, backupCount=10, maxBytes=5242880)
+    logger.addHandler(handler)
+
+
+def setup(check_args={}):
+    setup_logging()
+
     os.environ["SUGAR_DEVELOPER"] = "1"
 
-    config_args = get_config_args(log_name)
+    config_args = get_config_args()
 
     if is_buildbot():
         check_args["interactive"] = False
